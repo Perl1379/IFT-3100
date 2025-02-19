81287e9ae512b1f832d7bef4107532037f3b38ac
cobolfoo <ybourbeau@gmail.com>
2025-02-19 00:15:11 -0500

    - ModalDialog : The default modal dialog that show a generic message - Vector3Dialog : Used to pick X,Y,Z values for transform (position, orientation and scale)

---
+++ src/ui/Vector3Dialog.cpp
@@ -0,0 +1,75 @@
+/*****************************************************
+* TP IFT3100H24 - Knight Maker
+ * by Team 12
+ *****************************************************
+ *
+ * Vector3Dialog class implementation
+ *
+ *****************************************************/
+#include "Vector3Dialog.h"
+#include <imgui.h>
+
+
+/**
+ * Constructor
+ */
+Vector3Dialog::Vector3Dialog() : ModalDialog() {
+}
+
+
+/**
+ * Set attribute to modify
+ */
+void Vector3Dialog::useProperty(BaseNode* p_node, const std::string& p_name, glm::vec3 p_value) {
+
+    m_node = p_node;
+    m_name = p_name;
+    m_value = p_value;
+
+}
+
+/**
+ * Draw dialog content
+ */
+void Vector3Dialog::draw() {
+
+    if (ImGui::BeginPopupModal(m_title.c_str(), NULL, ImGuiWindowFlags_AlwaysAutoResize))
+    {
+        ImGui::Text("X:");
+        ImGui::SameLine();
+        ImGui::InputFloat("##X", &m_value.x);
+        ImGui::Text("Y:");
+        ImGui::SameLine();
+        ImGui::InputFloat("##Y", &m_value.y);
+        ImGui::Text("Z:");
+        ImGui::SameLine();
+        ImGui::InputFloat("##Z", &m_value.z);
+
+        ImGui::Dummy(ImVec2(0.0f, 5.0f));
+        ImGui::Separator();
+        ImGui::Dummy(ImVec2(0.0f, 5.0f));
+
+        if (ImGui::Button("Apply Changes"))
+        {
+            if (m_title == "Change Position") {
+                m_node->setProperty("Position", m_value);
+            } else if (m_title == "Change Orientation") {
+                m_node->setProperty("Orientation", m_value);
+            } else if (m_title == "Change Scale"){
+                m_node->setProperty("Scale", m_value);
+            }
+            ImGui::CloseCurrentPopup();
+            m_isOpen = false;
+        }
+
+        ImGui::SameLine();
+
+        if (ImGui::Button("Cancel"))
+        {
+            ImGui::CloseCurrentPopup();
+            m_isOpen = false;
+        }
+
+        ImGui::EndPopup();
+    }
+}
---
+++ src/ui/Vector3Dialog.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <BaseNode.h>
+
+#include "ModalDialog.h"
+
+
+class Vector3Dialog : public ModalDialog {
+
+private:
+    BaseNode* m_node;
+    std::string m_name;
+    glm::vec3 m_value;
+
+
+
+  public:
+    Vector3Dialog();
+    void useProperty(BaseNode* p_node, const std::string& p_name, glm::vec3 p_value);
+    void draw() override;
+
+};
+
